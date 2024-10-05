deba29f1e6bf73103559cbaecb03f89a46b3fb49
António Macave <antonio.macave@gmail.com>
2024-10-05 13:28:45 +0200

    Initial commit

--- app/src/main/java/mz/co/macave/mundodoscarros/MainActivity.kt
+++ app/src/main/java/mz/co/macave/mundodoscarros/MainActivity.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
