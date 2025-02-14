b89c93bc0376a0d8668a57bf68582ee1dee0b598
António Macave <antonio.macave@gmail.com>
2025-02-14 13:52:51 +0200

    Dialog Box redesigned

--- app/src/main/java/mz/co/macave/mundodoscarros/screens/CurrenciesDialog.kt
+++ app/src/main/java/mz/co/macave/mundodoscarros/screens/CurrenciesDialog.kt
@@ -1,18 +1,33 @@
 package mz.co.macave.mundodoscarros.screens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 
 @Composable
@@ -23,11 +38,14 @@ fun CurrenciesDialog(
     Dialog(
         onDismissRequest = { onDismissRequest() },
     ) {
+
+        var (selected, onSelected) = remember { mutableStateOf("") }
+
         Card(
             modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(0.9f),
-            shape = CardDefaults.shape,
+                .height(500.dp)
+                .fillMaxWidth(),
+            shape = AlertDialogDefaults.shape,
             colors = CardDefaults.cardColors(),
             elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
             onClick = {
@@ -35,50 +53,95 @@ fun CurrenciesDialog(
             }
         ) {
 
-            Column {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
                 Column {
                     Text(
-                        text = "Title",
-                        style = MaterialTheme.typography.titleMedium
+                        text = "Currencies",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .fillMaxWidth()
                     )
                 }
-
-                CurrencyDialogItem(currency = "USD", country = "United States of America")
-                CurrencyDialogItem(currency = "MZN", country = "Mozambique")
-                CurrencyDialogItem(currency = "BRL", country = "Brazil")
-                CurrencyDialogItem(currency = "ZAR", country = "South Africa")
-                CurrencyDialogItem(currency = "EUR", country = "Portugal")
-                CurrencyDialogItem(currency = "GBP", country = "London")
-                CurrencyDialogItem(currency = "KWZ", country = "Angola")
+                HorizontalDivider()
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    CurrencyDialogItem(selected = true, currency = "USD", country = "United States")
+                    CurrencyDialogItem(selected = false, currency = "MZN", country = "Mozambique")
+                    CurrencyDialogItem(selected = false, currency = "BRL", country = "Brazil")
+                    CurrencyDialogItem(selected = false, currency = "ZAR", country = "South Africa")
+                    CurrencyDialogItem(selected = false, currency = "EUR", country = "Portugal")
+                    CurrencyDialogItem(selected = false, currency = "GBP", country = "London")
+                    CurrencyDialogItem(selected = false, currency = "KWZ", country = "Angola")
+                    CurrencyDialogItem(selected = false, currency = "CAD", country = "Canada")
+                    CurrencyDialogItem(selected = false, currency = "JPY", country = "Japan")
+                    CurrencyDialogItem(selected = false, currency = "YEN", country = "China")
+                }
+                HorizontalDivider()
+                DialogButtons(onDismissRequest)
             }
+
         }
-
     }
-
 }
 
 @Composable
-fun CurrencyDialogItem(currency: String, country: String) {
+fun DialogButtons(onDismissRequest: () -> Unit) {
+    Row (
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.End
+    ) {
+        TextButton(onClick = { onDismissRequest() }) {
+            Text(text = stringResource(id = android.R.string.cancel))
+        }
 
-    val modifier = Modifier
-        .padding(horizontal = 8.dp)
+        TextButton(onClick = { onDismissRequest() }) {
+            Text(text = stringResource(id = android.R.string.ok))
+        }
+    }
+}
 
-    Column(
+@Composable
+fun CurrencyDialogItem(selected: Boolean, currency: String, country: String) {
+
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
+            .clickable { },
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            modifier = modifier,
-            text = currency,
-            style = MaterialTheme.typography.labelMedium
+
+        RadioButton(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterVertically)
+            ,
+            selected = selected,
+            enabled = true,
+            onClick = {  }
         )
-        Spacer(modifier = Modifier.height(2.dp))
-        Text(
-            modifier = modifier,
-            text = country,
-            style = MaterialTheme.typography.bodySmall
-        )
+
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = currency,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = country,
+                fontSize = 14.sp
+            )
+        }
     }
 }
 
@@ -86,7 +149,7 @@ fun CurrencyDialogItem(currency: String, country: String) {
 @Composable
 fun DialogPreviews() {
     CurrenciesDialog(
-        onItemClick = {},
-        onDismissRequest = {}
+        onDismissRequest = {},
+        onItemClick = {}
     )
 }
