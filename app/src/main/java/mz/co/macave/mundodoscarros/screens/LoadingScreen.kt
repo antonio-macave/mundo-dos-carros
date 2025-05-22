148727f51a1b18bac49804e5feb38137885985de
António Macave <antonio.macave@gmail.com>
2025-05-22 12:25:05 +0200

    Changing ProgressIndicator to LoadingIndicator from M3 Expressive

--- app/src/main/java/mz/co/macave/mundodoscarros/screens/LoadingScreen.kt
+++ app/src/main/java/mz/co/macave/mundodoscarros/screens/LoadingScreen.kt
@@ -2,14 +2,22 @@ package mz.co.macave.mundodoscarros.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import mz.co.macave.mundodoscarros.R
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun LoadingScreen() {
     Column(
@@ -19,6 +27,8 @@ fun LoadingScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        CircularProgressIndicator()
+        LoadingIndicator(modifier = Modifier.size(80.dp))
+        Spacer(Modifier.height(16.dp))
+        Text(text = stringResource(R.string.loading))
     }
 }
