e97de4dd45674592a7f30080fdacdc22c9d53498
António Macave <antonio.macave@gmail.com>
2025-08-22 22:28:39 +0200

    M3 list item shape implemented

--- app/src/main/java/mz/co/macave/mundodoscarros/screens/AnosScreen.kt
+++ app/src/main/java/mz/co/macave/mundodoscarros/screens/AnosScreen.kt
@@ -1,9 +1,13 @@
 package mz.co.macave.mundodoscarros.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
@@ -12,17 +16,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import mz.co.macave.mundodoscarros.models.Ano
+import mz.co.macave.mundodoscarros.ui.theme.OnScreenBackGroundContainer
+import mz.co.macave.mundodoscarros.ui.theme.ScreenBackground
 
 
 @Composable
 fun AnosList(anosList: List<Ano>, onAnoClickItem: (Ano) -> Unit) {
     LazyColumn(
-        contentPadding = PaddingValues(12.dp)
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(color = ScreenBackground),
+        contentPadding = PaddingValues(all = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
-        items(items = anosList) {item ->
-            AnoItem(ano = item, onAnoClickItem = onAnoClickItem)
+        itemsIndexed(items = anosList) { index, _item ->
+            ShapedAnoItems(
+                items = anosList,
+                index = index,
+                onItemClick = onAnoClickItem
+            )
         }
     }
+}
+
+@Composable
+fun ShapedAnoItems(items: List<Ano>, index: Int, onItemClick: (Ano) -> Unit) {
+    ListItem(
+        modifier = Modifier
+            .clip(
+                shape = when (index) {
+                    0 -> RoundedCornerShape(
+                        topStart = 16.dp,
+                        topEnd = 16.dp,
+                        bottomStart = 4.dp,
+                        bottomEnd = 4.dp
+                    )
+                    items.lastIndex -> RoundedCornerShape(
+                        topStart = 4.dp,
+                        topEnd = 4.dp,
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                    else -> RoundedCornerShape(
+                        topStart = 4.dp,
+                        topEnd = 4.dp,
+                        bottomStart = 4.dp,
+                        bottomEnd = 4.dp
+                    )
+                }
+            )
+            .background(color = OnScreenBackGroundContainer)
+            .clickable {
+                onItemClick(items[index])
+            },
+        headlineContent = {
+            Text(
+                text = items[index].nome,
+                modifier = Modifier.padding(
+                    horizontal = 8.dp,
+                    vertical = 2.dp
+                )
+            )
+        },
+    )
 }
 
 @Composable
