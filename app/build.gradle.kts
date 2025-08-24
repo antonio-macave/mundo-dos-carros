9e0336dbbe36485d2f412a24854ff22e1444a078
António Macave <antonio.macave@gmail.com>
2025-08-24 12:36:46 +0200

    Version code and version name upgraded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "mz.co.macave.mundodoscarros"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
